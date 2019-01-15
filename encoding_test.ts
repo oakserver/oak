2c1710ca4cc2265cf54d9a29a3ec82c6cabd0137
Kitson Kelly <me@kitsonkelly.com>
2019-01-15 21:54:14 +1100

    Add tests for encoding

--- encoding_test.ts
+++ encoding_test.ts
@@ -1,5 +1,20 @@
 import { test, assertEqual } from "https://deno.land/x/std/testing/mod.ts";
+import { preferredEncodings } from "./encoding.ts";
 
-test(function test() {
-  assertEqual("", "");
+test(function encoding() {
+  assertEqual(preferredEncodings("gzip, compress;q=0.2, identity;q=0.5"), [
+    "gzip",
+    "identity",
+    "compress"
+  ]);
+});
+
+test(function availableEncoding() {
+  assertEqual(
+    preferredEncodings("gzip, compress;q=0.2, identity;q=0.5", [
+      "identity",
+      "gzip"
+    ]),
+    ["gzip", "identity"]
+  );
 });
