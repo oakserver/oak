2462a7c0e94602c486dc5e8c036cb5f9032cd287
Kitson Kelly <me@kitsonkelly.com>
2019-01-15 21:54:33 +1100

    Update tests for httpError

--- httpError_test.ts
+++ httpError_test.ts
@@ -1,9 +1,14 @@
-import { test, assertEqual } from "https://deno.land/x/std/testing/mod.ts";
+import {
+  test,
+  assert,
+  assertEqual
+} from "https://deno.land/x/std/testing/mod.ts";
 
-import * as httpError from "./httpError.ts";
+import httpError, { createHttpError } from "./httpError.ts";
 
 test(function createHttpErrorTest() {
-  const err = httpError.createHttpError(501);
+  const err = createHttpError(501);
+  assert(err instanceof httpError.NotImplemented);
   assertEqual(err.status, 501);
   assertEqual(err.name, "NotImplementedError");
   assertEqual(err.message, "Not Implemented");
