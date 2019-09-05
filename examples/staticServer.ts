8478d62216961d083461ca13052208534dd72c81
Kitson Kelly <me@kitsonkelly.com>
2019-09-05 16:39:46 +1000

    Update CI to Deno 0.17

--- examples/staticServer.ts
+++ examples/staticServer.ts
@@ -4,7 +4,7 @@ import {
   bold,
   yellow,
   red
-} from "https://deno.land/std@v0.16.0/fmt/colors.ts";
+} from "https://deno.land/std@v0.17.0/fmt/colors.ts";
 
 import { Application, HttpError, send, Status } from "../mod.ts";
 
