4da3a3ff0987d4910e4ee63f0f9b7d688bb811fc
EnokMan <416828041@qq.com>
2019-11-15 13:24:43 +0800

    Bump to deno  v0.24.0 (#25)

--- examples/server.ts
+++ examples/server.ts
@@ -9,7 +9,7 @@ import {
   cyan,
   bold,
   yellow
-} from "https://deno.land/std@v0.21.0/fmt/colors.ts";
+} from "https://deno.land/std@v0.24.0/fmt/colors.ts";
 
 import { Application } from "../mod.ts";
 
