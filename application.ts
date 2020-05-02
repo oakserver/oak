583822680122d88b31656611b95110634769ba6e
EnokMan <416828041@qq.com>
2020-05-01 21:55:56 -0500

    Resolving Request Deadlocks (#41)
    Fixes #40

--- application.ts
+++ application.ts
@@ -2,8 +2,7 @@
 
 import { Context } from "./context.ts";
 import { compose, Middleware } from "./middleware.ts";
-import { HTTPSOptions, serve, serveTLS } from "./deps.ts";
-
+import { HTTPSOptions, serve, serveTLS, ServerRequest } from "./deps.ts";
 /** A class which registers middleware (via `.use()`) and then processes
  * inbound requests against that middleware (via `.listen()`).
  *
@@ -22,15 +21,23 @@ export class Application<S extends object = { [key: string]: any }> {
 
   constructor(private _serve = serve, private _serveTLS = serveTLS) {}
 
+  /** Processing registered middleware on each request. */
+  private async _handleRequest(
+    request: ServerRequest,
+    middleware: (context: Context<S>) => Promise<void>
+  ) {
+    const context = new Context<S>(this, request);
+    await middleware(context);
+    await request.respond(context.response.toServerResponse());
+  }
+
   /** Start listening for requests over HTTP, processing registered middleware
    * on each request. */
   async listen(addr: string | Deno.ListenOptions): Promise<void> {
     const middleware = compose<S, Context<S>>(this._middleware);
     const server = this._serve(addr);
     for await (const request of server) {
-      const context = new Context<S>(this, request);
-      await middleware(context);
-      await request.respond(context.response.toServerResponse());
+      this._handleRequest(request, middleware);
     }
   }
 
@@ -40,9 +47,7 @@ export class Application<S extends object = { [key: string]: any }> {
     const middleware = compose<S, Context<S>>(this._middleware);
     const server = this._serveTLS(options);
     for await (const request of server) {
-      const context = new Context<S>(this, request);
-      await middleware(context);
-      await request.respond(context.response.toServerResponse());
+      this._handleRequest(request, middleware);
     }
   }
 
