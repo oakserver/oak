850b6409f62ab39d2a1555a56335103191581f7e
Kitson Kelly <me@kitsonkelly.com>
2020-05-05 10:42:15 +1000

    Update examples to Deno 1.0.0-rc1

--- examples/staticServer.ts
+++ examples/staticServer.ts
@@ -4,7 +4,7 @@ import {
   bold,
   yellow,
   red,
-} from "https://deno.land/std@v0.39.0/fmt/colors.ts";
+} from "https://deno.land/std@v1.0.0-rc1/fmt/colors.ts";
 
 import { Application, HttpError, send, Status } from "../mod.ts";
 
