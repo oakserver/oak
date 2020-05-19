6725cd56e484d25c52b4bcf94171bd4cc4eaac34
Kitson Kelly <me@kitsonkelly.com>
2020-05-20 08:41:20 +1000

    Add Deno.Reader example

---
+++ examples/readerServer.ts
@@ -0,0 +1,27 @@
+/*
+ * This is a basic example of a test server which uses a `Deno.Reader` as the
+ * body response.
+ */
+
+// Importing some console colors
+import {
+  bold,
+  yellow,
+} from "https://deno.land/std@0.51.0/fmt/colors.ts";
+import { StringReader } from "https://deno.land/std@0.51.0/io/readers.ts";
+
+import { Application } from "../mod.ts";
+
+const app = new Application();
+
+app.use(async (ctx) => {
+  ctx.response.body = new StringReader("Hello, Reader!");
+  ctx.response.type = "text/plain";
+});
+
+const options = { hostname: "127.0.0.1", port: 8000 };
+console.log(
+  bold("Start listening on ") + yellow(`${options.hostname}:${options.port}`),
+);
+await app.listen(options);
+console.log(bold("Finished."));
