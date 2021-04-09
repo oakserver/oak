c04cf3818cb36bb58b06351353864be21ce877a8
KotlinIsland <65446343+KotlinIsland@users.noreply.github.com>
2021-04-09 10:45:45 +1000

    Update to Deno 1.8.3, std 0.92.0 (#288)
    Co-authored-by: KotlinIsland <asdf>

--- examples/closeServer.ts
+++ examples/closeServer.ts
@@ -8,7 +8,7 @@ import {
   cyan,
   green,
   yellow,
-} from "https://deno.land/std@0.84.0/fmt/colors.ts";
+} from "https://deno.land/std@0.92.0/fmt/colors.ts";
 
 import { Application, Context, Router, Status } from "../mod.ts";
 
