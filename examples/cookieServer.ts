682594091e065a20ec01bd70f32755cf2d2dc00e
Kitson Kelly <me@kitsonkelly.com>
2021-05-13 10:21:33 +1000

    chore: udpate to Deno 1.10.1, std 0.96.0, media_types 2.8.4

--- examples/cookieServer.ts
+++ examples/cookieServer.ts
@@ -8,7 +8,7 @@ import {
   cyan,
   green,
   yellow,
-} from "https://deno.land/std@0.95.0/fmt/colors.ts";
+} from "https://deno.land/std@0.96.0/fmt/colors.ts";
 
 import { Application } from "../mod.ts";
 
