4333736942afe73b4b379fcd365e48038f95fc13
David Sherret <dsherret@gmail.com>
2022-06-15 15:48:47 -0400

    Fixed.

--- request_test.ts
+++ request_test.ts
@@ -284,6 +284,13 @@ test({
 test({
   name: "Request - inspecting",
   fn() {
+    console.log(Deno.inspect(
+      new Request(
+        createMockNativeRequest("http://localhost/foo?bar=baz&qat=qux", {
+          headers: { host: "localhost" },
+        }),
+      ),
+    ));
     assertEquals(
       Deno.inspect(
         new Request(
@@ -293,7 +300,7 @@ test({
         ),
       ),
       isNode()
-        ? `Request {\n  hasBody: false,\n  headers: HeadersList(2) [ 'host', 'localhost' ],\n  ip: '',\n  ips: [],\n  method: 'GET',\n  secure: false,\n  url: URL {\n    href: 'http://localhost/foo?bar=baz&qat=qux',\n    origin: 'http://localhost',\n    protocol: 'http:',\n    username: '',\n    password: '',\n    host: 'localhost',\n    hostname: 'localhost',\n    port: '',\n    pathname: '/foo',\n    search: '?bar=baz&qat=qux',\n    searchParams: URLSearchParams { 'bar' => 'baz', 'qat' => 'qux' },\n    hash: ''\n  }\n}`
+        ? `Request {\n  hasBody: false,\n  headers: HeadersList {\n    [Symbol(headers map)]: [Map],\n    [Symbol(headers map sorted)]: null\n  },\n  ip: '',\n  ips: [],\n  method: 'GET',\n  secure: false,\n  url: URL {\n    href: 'http://localhost/foo?bar=baz&qat=qux',\n    origin: 'http://localhost',\n    protocol: 'http:',\n    username: '',\n    password: '',\n    host: 'localhost',\n    hostname: 'localhost',\n    port: '',\n    pathname: '/foo',\n    search: '?bar=baz&qat=qux',\n    searchParams: URLSearchParams { 'bar' => 'baz', 'qat' => 'qux' },\n    hash: ''\n  }\n}`
         : `Request {\n  hasBody: false,\n  headers: Headers { host: "localhost" },\n  ip: "",\n  ips: [],\n  method: "GET",\n  secure: false,\n  url: "http://localhost/foo?bar=baz&qat=qux"\n}`,
     );
   },
