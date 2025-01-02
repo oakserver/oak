d9822408e2a8f1f7f24212c0ce6269a433f1d00c
Kitson Kelly <me@kitsonkelly.com>
2025-01-03 08:10:29 +1100

    chore: cleanup dependencies

--- deps_test.ts
+++ deps_test.ts
@@ -6,3 +6,4 @@ export { assertRejects } from "jsr:@std/assert@^1.0/rejects";
 export { assertStrictEquals } from "jsr:@std/assert@^1.0/strict-equals";
 export { assertThrows } from "jsr:@std/assert@^1.0/throws";
 export { unreachable } from "jsr:@std/assert@^1.0/unreachable";
+export { timingSafeEqual } from "jsr:@std/crypto@^1.0/timing-safe-equal";
